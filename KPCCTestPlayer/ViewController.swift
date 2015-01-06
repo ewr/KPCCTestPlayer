3a0444d9d0af69a2aa05905cd347a982326d447f
Eric Richardson <e@ericrichardson.com>
2015-01-06 15:14:11 -0500

    Initial Commit

---
+++ KPCCTestPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KPCCTestPlayer
+//
+//  Created by Eric Richardson on 1/6/15.
+//  Copyright (c) 2015 Eric Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
