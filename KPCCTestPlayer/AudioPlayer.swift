d64b3acdf5a6a43236f482891e44463ff4bb819c
Eric Richardson <e@ericrichardson.com>
2015-01-24 12:31:47 -0500

    AudioPlayer Status enum; Load current program info on startup

--- KPCCTestPlayer/AudioPlayer.swift
+++ KPCCTestPlayer/AudioPlayer.swift
@@ -19,54 +19,82 @@ class AudioPlayer {
     
     //----------
     
-    let STREAM_URL = "http://streammachine-hls001.scprdev.org/sg/kpcc-aac.m3u8"
+    enum Statuses {
+        case New, Stopped, Playing, Seeking, Paused, Error
+        
+        func toString() -> String {
+            switch self {
+            case New:
+                return "New"
+            case Playing:
+                return "Playing"
+            case Stopped:
+                return "Stopped"
+            case Paused:
+                return "Paused"
+            case Seeking:
+                return "Seeking"
+            case Error:
+                return "Error"
+            }
+        }
+    }
+
+    //----------
+
+    let STREAM_URL = "http://streammachine-hls001.scprdev.org/sg/kpcc-aac.m3u8?ua=KPCC-EWRTest"
     
     let NORMAL_REWIND = 4 * 60 * 60
-    
+
     var _player: AVPlayer?
-    
+
     var playing: Bool
-    
+
     var _timeObserver: AnyObject?
-    
+
     var _dateFormat: NSDateFormatter
-    
+
     struct StreamDates {
         var curDate: NSDate
         var minDate: NSDate?
         var maxDate: NSDate?
     }
-    
+
     var currentDates: StreamDates?
-    
+
     var _observers: [(StreamDates) -> Void] = []
     var _showObservers: [(Schedule.ScheduleInstance?) -> Void] = []
-    
+    var _statusObservers: [(Statuses) -> Void] = []
+
     var _currentShow: Schedule.ScheduleInstance? = nil
     var _checkingDate: NSDate?
     var _seeking: Bool = false
     
+    var status: Statuses = Statuses.New
+
     //----------
-    
+
     init() {
         self.playing = false
-        
+
         self._dateFormat = NSDateFormatter()
         self._dateFormat.dateFormat = "hh:mm:ss a"
+        
+        
     }
-    
+
     //----------
-    
+
     func getPlayer() -> AVPlayer {
         if (self._player == nil) {
             self._player = AVPlayer(URL:NSURL(string:self.STREAM_URL))
-            
+
             let av = AVAudioSession.sharedInstance()
             av.setCategory(AVAudioSessionCategoryPlayback, error:nil)
-            
+
             // FIXME: should be checking return here to see if we did go active
             av.setActive(true, error: nil)
-            
+
             // observe time every second
             self._player?.addPeriodicTimeObserverForInterval(CMTimeMake(1,1), queue: nil,
                 usingBlock: {(time:CMTime) in
@@ -74,132 +102,159 @@ class AudioPlayer {
                         // we don't want to update anything mid-seek
                         return
                     }
-                    
+
                     var curDate = self._player!.currentItem.currentDate()
-                    
+
                     var seek_range: CMTimeRange
                     var minDate: NSDate? = nil
                     var maxDate: NSDate? = nil
-                    
+
                     if !self._player!.currentItem.seekableTimeRanges.isEmpty {
                         seek_range = self._player!.currentItem.seekableTimeRanges[0].CMTimeRangeValue
-                        
+
                         minDate = NSDate(timeInterval: -1 * (CMTimeGetSeconds(time) - CMTimeGetSeconds(seek_range.start)), sinceDate:curDate)
                         maxDate = NSDate(timeInterval: CMTimeGetSeconds(CMTimeRangeGetEnd(seek_range)) - CMTimeGetSeconds(time), sinceDate:curDate)
-                        
+
 
                         //NSLog("minDate is %@", self._dateFormat.stringFromDate(minDate))
                         //NSLog("maxDate is %@", self._dateFormat.stringFromDate(maxDate))
                     }
-                    
+
                     var status = StreamDates(curDate: curDate, minDate: minDate, maxDate: maxDate)
-                    
+
                     self.currentDates = status
-                    
+
                     for o in self._observers {
                         o(status)
                     }
-                    
+
                     NSLog("curDate is %@", self._dateFormat.stringFromDate(curDate))
-                    
+
                     self._checkForNewShow(curDate, from_seek:false)
                 }
             )
         }
-        
+
         return self._player!
-        
+
     }
     
     //----------
     
+    private func _setStatus(s:Statuses) -> Void {
+        let shouldNotify:Bool = !(self.status == s)
+        self.status = s
+        
+        if shouldNotify {
+            for o in self._statusObservers {
+                o(s)
+            }
+        }
+    }
+
+    //----------
+
     func observeTime(observer:(StreamDates) -> Void) -> Void {
         self._observers.append(observer)
     }
-    
+
     //----------
-    
+
     func onShowChange(observer:(Schedule.ScheduleInstance?) -> Void) -> Void {
         self._showObservers.append(observer)
     }
     
     //----------
     
+    func onStatusChange(observer:(Statuses) -> Void) -> Void {
+        self._statusObservers.append(observer)
+    }
+
+    //----------
+
     func play() -> Bool{
         self.getPlayer().play()
         self.playing = true
-        
+        self._setStatus(Statuses.Playing)
+
         return true
     }
-    
+
     //----------
-    
+
     func pause() -> Bool {
         self.getPlayer().pause()
         self.playing = false
-    
+        self._setStatus(Statuses.Paused)
+
         return true
     }
-    
+
     //----------
-    
+
     func stop() -> Bool {
         // FIXME: tear down player
-        
+
         self.currentDates = nil
-        
+        self._setStatus(Statuses.Stopped)
+
         return true
     }
-    
+
     //----------
-    
+
     func seekToDate(date: NSDate) -> Bool {
         // do we think we can do this?
         // FIXME: check currentDates if we have them
         NSLog("seekToDate called for %@",self._dateFormat.stringFromDate(date))
-        
+
         let p = self.getPlayer()
-        
+
         self._seeking = true
+        self._setStatus(Statuses.Seeking)
+        
         p.currentItem.seekToDate(date, completionHandler: { finished in
             if finished {
                 NSLog("seekToDate landed at %@", self._dateFormat.stringFromDate(p.currentItem.currentDate()))
                 self._seeking = false
+                self._setStatus(Statuses.Playing)
                 // FIXME: Need to see if we landed where we should have. If not, try again
             } else {
                 NSLog("seekToDate did not finish")
             }
-            
+
 
         })
-        
+
         return true
     }
-    
+
     //----------
-    
+
     func seekToPercent(percent: Float64) -> Bool {
         let p = self.getPlayer()
-        
+
         var seek_range = p.currentItem.seekableTimeRanges[0].CMTimeRangeValue
-        
+
         var seek_time = CMTimeAdd( seek_range.start, CMTimeMultiplyByFloat64(seek_range.duration,percent))
-        
+
         self._seeking = true
+        self._setStatus(Statuses.Seeking)
         p.currentItem.seekToTime(seek_time, completionHandler: {(finished:Bool) -> Void in
             if finished {
                 NSLog("seekToPercent landed from %2f", percent)
                 self._seeking = false
+                self._setStatus(Statuses.Playing)
             }
-            
+
 
         })
-        
+
         return true
     }
-    
+
     //----------
-    
+
     func _checkForNewShow(date:NSDate,from_seek:Bool = false) -> Void {
         if self._currentShow != nil && (
             (date.timeIntervalSinceReferenceDate >= self._currentShow!.starts_at.timeIntervalSinceReferenceDate)
@@ -208,35 +263,35 @@ class AudioPlayer {
             // we're still in our current show... no change
             return
         }
-        
+
         // we either don't have a current show, or we're no longer inside it
-        
+
         if self._checkingDate != nil && !from_seek {
             // we don't interrupt for normal ticks
             return
         }
-        
+
         // we should fetch for our new time
         self._checkingDate = date
-        
+
         Schedule.sharedInstance.at(date) { show in
             // make sure a different fetch didn't fire while we were waiting
             if self._checkingDate == date {
                 self._currentShow = show
                 self._checkingDate = nil
-                
+
                 if show != nil {
                     NSLog("Current show is %@",self._currentShow!.title)
                 } else {
                     NSLog("_checkForNewShow failed to get show")
                 }
-                
+
                 // update any observers
                 for o in self._showObservers {
                     o(show)
                 }
             }
         }
-        
+
     }
 }
--- KPCCTestPlayer/ViewController.swift
+++ KPCCTestPlayer/ViewController.swift
@@ -16,57 +16,96 @@ class ViewController: UIViewController {
     @IBOutlet weak var showLabel: UILabel!
     @IBOutlet weak var showTimes: UILabel!
     @IBOutlet weak var rewindButton: UIButton!
+    
+    var currentShow:Schedule.ScheduleInstance?
+    
+    let _timeF = NSDateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         self.playPauseButton.addTarget(self, action: "playPauseTapped:", forControlEvents: UIControlEvents.TouchUpInside)
         self.progressSlider.addTarget(self, action: "sliderUpdated:", forControlEvents: UIControlEvents.ValueChanged)
         self.rewindButton.addTarget(self, action: "rewindTapped:", forControlEvents: UIControlEvents.TouchUpInside)
-        
+
         var formatter = NSDateFormatter()
         formatter.dateFormat = "YYYY-MM-DD hh:mm:ss"
-        
-        var _timeF = NSDateFormatter()
-        _timeF.dateFormat = "h:mma"
-        
+
+        self._timeF.dateFormat = "h:mma"
+
         //---
-        
+
         AudioPlayer.sharedInstance.observeTime() { (status:AudioPlayer.StreamDates) -> Void in
             // set current time display
             self.timeDisplay.text = formatter.stringFromDate(status.curDate)
-            
+
             // set slider
             if status.minDate != nil {
                 var duration: Double = status.maxDate!.timeIntervalSince1970 - status.minDate!.timeIntervalSince1970
                 var position: Double = status.curDate.timeIntervalSince1970 - status.minDate!.timeIntervalSince1970
-                
+
                 var percent = position / duration
-                
+
                 self.progressSlider.value = Float(percent)
             }
+        }
+
+        //---
+
+        AudioPlayer.sharedInstance.onShowChange() { show in
+            self.setShowInfoFromShow(show)
         }
         
         //---
         
-        AudioPlayer.sharedInstance.onShowChange() { show in
-            if show != nil {
-                self.showLabel.text = show!.title
-                self.showTimes.text = _timeF.stringFromDate(show!.starts_at) + " - " + _timeF.stringFromDate(show!.ends_at)
-                
-            } else {
-                self.showLabel.text = "????"
-                self.showTimes.text = ""
+        AudioPlayer.sharedInstance.onStatusChange() { status in
+            NSLog("view player status is %@",status.toString())
+            
+            switch status {
+            case AudioPlayer.Statuses.New:
+                true
+            case AudioPlayer.Statuses.Playing:
+                // do something
+                true
+            case AudioPlayer.Statuses.Stopped:
+                // do something
+                true
+            case AudioPlayer.Statuses.Paused:
+                // do something
+                true
+            default:
+                // we don't care...
+                true
             }
         }
+        
+        // -- load information for what's on now -- //
+        
+        Schedule.sharedInstance.at(NSDate()) { show in
+            self.setShowInfoFromShow(show)
+        }
     }
-    
+
     //----------
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    //----------
+    
+    func setShowInfoFromShow(show:Schedule.ScheduleInstance?) -> Void {
+        if show != nil {
+            self.showLabel.text = show!.title
+            self.showTimes.text =
+                self._timeF.stringFromDate(show!.starts_at) + " - " + self._timeF.stringFromDate(show!.ends_at)
+            
+        } else {
+            self.showLabel.text = "????"
+            self.showTimes.text = ""
+        }
     }
     
     //----------
@@ -81,19 +120,19 @@ class ViewController: UIViewController {
             ap.play()
         }
     }
-    
+
     //----------
-    
+
     func rewindTapped(sender:UIButton!) {
         let ap = AudioPlayer.sharedInstance
-        
+
         if ap._currentShow != nil {
             ap.seekToDate(ap._currentShow!.soft_starts_at)
         }
     }
-    
+
     //----------
-    
+
     func sliderUpdated(sender:UISlider) {
         let ap = AudioPlayer.sharedInstance
         var fpercent = Float64(sender.value)
